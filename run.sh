6ff4bf133aa974d41220fc560ab35b8e9c990d68
Henrique Fan <fanhenrique@gmail.com>
2025-06-07 15:24:17 -0300

    run script

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source venv/bin/activate
+
+python main.py --sig \
+    ecdsa \
+    mldsa \
+    dilithium \
+    sphincs-sha-s \
+    sphincs-sha-f \
+    sphincs-shake-s \
+    sphincs-shake-f \
+    falcon \
+    falcon-padded \
+    mayo \
+    cross-rsdp-small \
+    cross-rsdpg-small \
+    cross-rsdp-balanced \
+    cross-rsdpg-balanced \
+    cross-rsdp-fast \
+    cross-rsdpg-fast \
+    --runs 10000 \
+    --warm-up 1000 \
+    --levels 1 2 3 4 5 \
+    --runs-simulator 1000
+
